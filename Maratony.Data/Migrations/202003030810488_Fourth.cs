ce604e4fe6274ed434265bd95db1bdc1b371b8aa
Marcin Wołkowicz <mwolkowi17@hotmail.com>
2020-03-03 13:28:25 +0100

    second doinh job

---
+++ Maratony.Data/Migrations/202003030810488_Fourth.cs
@@ -0,0 +1,46 @@
+namespace Maratony.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Third : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Biegaczs",
+                c => new
+                    {
+                        ID = c.Long(nullable: false, identity: true),
+                        Imie = c.String(),
+                        Nazwisko = c.String(),
+                        Czas = c.Time(nullable: false, precision: 7),
+                        Bieg = c.Long(nullable: false),
+                        Zawody_ID = c.Long(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Zawodies", t => t.Bieg)
+                .Index(t => t.Bieg);
+            
+            CreateTable(
+                "dbo.Zawodies",
+                c => new
+                    {
+                        ID = c.Long(nullable: false, identity: true),
+                        Miejsce = c.String(),
+                        Data = c.DateTime(nullable: false),
+                        Dystans = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Biegaczs", "Zawody_ID", "dbo.Zawodies");
+            DropIndex("dbo.Biegaczs", new[] { "Zawody_ID" });
+            DropTable("dbo.Zawodies");
+            DropTable("dbo.Biegaczs");
+        }
+    }
+}
